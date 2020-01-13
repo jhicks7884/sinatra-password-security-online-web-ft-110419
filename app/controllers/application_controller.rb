3668da351f0a2c373a79912563575093fcb27a21
Jeshua Hicks <jhicks7884@outlook.com>
2020-01-13 18:58:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ end
 
 	post "/login" do
 		user = User.find_by(:username => params[:username])
-  if user && user.authenticate(params[:password])
+  if user
     session[:user_id] = user.id
     redirect "/success"
   else
